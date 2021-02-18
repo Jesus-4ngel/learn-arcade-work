65f053604e2ba526500e9b33635567349560ee79
jesus-4ngel <jesus.hoyo@edu.uah.es>
2021-02-18 20:55:08 +0100

    Work on lab 1

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,2 +1,8 @@
-Hola Mundo
+print('Hello World')
+print('We are learning how to use arcade, and it does not seem to be too difficult to use.')
+print('By the time I am writing this, I have not had so much problems with it.')
+print('It may give me some problems in the future but I think I will be able to solve it.')
+print('If I cannot solve that problems, I will ask for some help.')
+print('\tI was asked to use a escape code, so here we are.')
+
 
