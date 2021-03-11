8bbbf5603758fc03b8cbc9245f954d4ce4ad9758
jesus-4ngel <jesus.hoyo@edu.uah.es>
2021-03-11 20:38:16 +0100

    # First Try

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -4,5 +4,5 @@ print('By the time I am writing this, I have not had so much problems with it.')
 print('It may give me some problems in the future but I think I will be able to solve it.')
 print('If I cannot solve that problems, I will ask for some help.')
 print('\tI was asked to use a escape code, so here we are.')
-
+# First Try
 
